787026a85873880f371b15c9b7de7d4cf6fd8634
SEONGAHG17 <94235278+SEONGAHG17@users.noreply.github.com>
2026-09-15 16:24:45 +0900

    Update day01.py

--- quiz/solution/day01.py
+++ quiz/solution/day01.py
@@ -1,3 +1,10 @@
+# Q.07 메로나 출력 
+print(f"이름\t\t가격\t재고\n{'-'*28}\n메로나\t\t{icecream['메로나'][0]}\t{icecream['메로나'][1]}")
+
+# Q.08 비비빅 출력 
+print(f"이름\t\t가격\t재고\n{'-'*28}\n비비빅\t\t{icecream['비비빅'][0]}\t{icecream['비비빅'][1]}")
+
+# Q.11 마방진
 magic_square = []
 
 for i in range(4): # 행 row
