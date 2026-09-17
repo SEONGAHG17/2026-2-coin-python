b8242763650853ccef783175002b3c76f6210549
SEONGAHG17 <94235278+SEONGAHG17@users.noreply.github.com>
2026-09-17 21:14:11 +0900

    rock_scissors_paper.py 업데이트

--- example/rock_scissors_paper.py
+++ example/rock_scissors_paper.py
@@ -1,3 +1,65 @@
+win = 0
+lose = 0
+draw = 0
+computer_number = 1
+
+while win < 3:
+    print("\n가위 / 바위 / 보")
+    user = input("당신의 선택: ")
+
+    # 잘못된 입력 검사 
+    if user not in ["가위", "바위", "보"]:
+        print("잘못된 입력입니다!")
+        continue
+
+    # 컴퓨터 선택 (1: 가위, 2: 바위, 3: 보)
+    if computer_number == 1:
+        computer = "가위"
+    elif computer_number == 2:
+        computer = "바위"
+    else:
+        computer = "보"
+
+    print("컴퓨터:", computer)
+
+    # 승부 판정
+    if user == computer:
+        print("비겼습니다!")
+        draw += 1
+    elif (user == "가위" and computer == "보") or 
+         (user == "바위" and computer == "가위") or 
+         (user == "보" and computer == "바위"):
+        print("당신이 이겼습니다!")
+        win += 1
+    else:
+        print("당신이 졌습니다!")
+        lose += 1
+
+    # 컴퓨터 다음 선택 순환 (1 -> 2 -> 3 -> 1)
+    computer_number = computer_number + 1
+    if computer_number > 3:
+        computer_number = 1
+
+print("\n당신이 3번 이겼어요!!\n가위바위보를 종료합니다.\n")
+print(f"최종 결과\n{win + lose + draw}전 {win}승 {lose}패 {draw}무")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # random 모듈 안쓰고!
 
 win = 0
