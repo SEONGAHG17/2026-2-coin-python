dc97bf6e755a102c01b2bec6eb69ce09996ccffc
SEONGAHG17 <94235278+SEONGAHG17@users.noreply.github.com>
2026-09-17 10:34:56 +0900

    Update assignment_operator.py

--- example/assignment_operator.py
+++ example/assignment_operator.py
@@ -8,13 +8,18 @@ print(num) # 100
 
 파이썬은 다중할당과 동시할당이 가능하다
 """
-# 다중 할당
+"""
+동일 할당 : 하나의 값을 여러 변수에 동일참조로 연속 연결 
+"""
+
+
+# 동일 할당, 연쇄 할당
 num1 = num2 = num3 = 300
 print(num1)
 print(num2)
 print(num3)
 
-# 동시 할당
+# 다중 할당, 동시 할당
 num4, num5 = 400, 500
 print(num4)
 print(num5)
@@ -34,18 +39,28 @@ print(mutable2)
 
 
 
+# packing, unpacking
+"""
+packing : 여러 데이터를 하나의 컨테이너(주로 튜플)로 묶는 것
+    - 파이썬에서 줄로 ,로 나열된 값들을 괄호 없이 적으면 자동으로 튜플로 패킹
+    - 여러 값 >> 하나의 묶음
+    
+    
+unpacking : 묶여 있는 컬렉션의 요소들을 각각 개발변수로 풀어내는 것
+    - 좌변의 변수 개수와 우변의 컬랙션 요소 개수가 정확히 일치해야한다
+    - 하나의 묶음 >> 여러 변수
+"""
+
 
 # 동시 할당 
 def get_min_max(numbers):
     # 최솟값과 최댓값을 동시에 반환
     return min(numbers), max(numbers)
-
-
-# packing, unpacking
-
-
 # 실습: 두 개의 변수에 동시에 나누어 담기
 low, high = get_min_max([15, 42, 8, 23, 99])
 
 print(low)   # 출력: 8
 print(high)  # 출력: 99
+
+
+
